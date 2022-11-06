e7ac9b5bae86001dda354419079e60c55db15743
San'Quan Prioleau <sq.prioleau@gmail.com>
2022-11-06 17:36:21 -0500

    👔 Add game logic and styles

---
+++ js/questionOptions.js
@@ -0,0 +1,52 @@
+export default [
+  {
+    id: 1,
+    correct: { symbol: 14 },
+    incorrect: { symbol: 14, flipped: true, },
+  },
+  {
+    id: 2,
+    correct: { symbol: 41 },
+    incorrect: { symbol: 14, flipped: false, },
+  },
+  {
+    id: 3,
+    correct: { symbol: 7 },
+    incorrect: { symbol: 7, flipped: true, },
+  },
+  {
+    id: 4,
+    correct: { symbol: 5 },
+    incorrect: { symbol: 2, flipped: false, },
+  },
+  {
+    id: 4,
+    correct: { symbol: 6 },
+    incorrect: { symbol: 9, flipped: false, },
+  },
+  {
+    id: 5,
+    correct: { symbol: 9 },
+    incorrect: { symbol: 6, flipped: true, },
+  },
+  {
+    id: 6,
+    correct: { symbol: "M" },
+    incorrect: { symbol: "W", flipped: false, },
+  },
+  {
+    id: 7,
+    correct: { symbol: "Z" },
+    incorrect: { symbol: "Z", flipped: true, },
+  },
+  {
+    id: 8,
+    correct: { symbol: "S" },
+    incorrect: { symbol: "Z", flipped: false, },
+  },
+  {
+    id: 9,
+    correct: { symbol: "S" },
+    incorrect: { symbol: 5, flipped: false, },
+  },
+];
